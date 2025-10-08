83c1fd92d51877373e9c4c30db09df6b9f84b677
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-08 12:19:12 +0200

    setting index.css
    roots ok
    starting page style

--- helpdesk/src/App.tsx
+++ helpdesk/src/App.tsx
@@ -1,5 +1,7 @@
+import { AppRoutes } from "./routes";
+
 function App() {
-  return <h1 className="text-cyan-800 text-7xl">Testando meu projeto</h1>;
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ helpdesk/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+export function Login() {
+  return (
+    <div className="w-full h-screen bg-login  bg-cover bg-center ">
+      <p>just testing again adasdasdad</p>
+    </div>
+  );
+}
