29e5f79de8432c24b4ac64c837142f542a1bbb79
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-08 12:38:49 +0200

    stting login page

--- helpdesk/src/pages/Login.tsx
+++ helpdesk/src/pages/Login.tsx
@@ -1,7 +1,28 @@
 export function Login() {
   return (
     <div className="w-full h-screen bg-login  bg-cover bg-center ">
-      <p>just testing again adasdasdad</p>
+      <div>
+        <div>logo</div>
+        <div>
+          <form action="">
+            <h1>Acess the portal</h1>
+            <p>Log in using your e-mail and password registered</p>
+
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" />
+
+            <label htmlFor="senha">Senha</label>
+            <input id="senha" type="password" />
+
+            <button type="submit">Enter</button>
+          </form>
+          <div>
+            <h1>Don't have an account yet?</h1>
+            <p>Register right now</p>
+            <button>Create Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
