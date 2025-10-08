7e651cd4fbcb7a990a42be0e886771e2a03ad840
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-08 16:56:19 +0200

    updated style

--- helpdesk/src/pages/Login.tsx
+++ helpdesk/src/pages/Login.tsx
@@ -1,12 +1,18 @@
+import headerLogoSvg from "../assets/images/NavHeader-logo.svg";
+
 export function Login() {
   return (
-    <div className="w-full h-screen bg-login  bg-cover bg-center ">
-      <div>
-        <div>logo</div>
-        <div>
-          <form action="">
-            <h1>Acess the portal</h1>
-            <p>Log in using your e-mail and password registered</p>
+    <div className="w-full h-screen bg-login  bg-cover bg-center flex items-end justify-end">
+      <div className="w-[50%] h-[98%] bg-[var(--gray-600)] rounded-tl flex flex-col items-center justify-start ">
+        <div className="flex flex-col items-center pt-13 gap-3">
+          <div className="mb-7">
+            <img src={headerLogoSvg} alt="Helpdesk purple iconlogo" />
+          </div>
+          <form className="w-[400px] h-[347px] border border-gray-200 flex flex-col items-left justify-start p-7 rounded">
+            <h1 className="text-xl font-bold">Acess the portal</h1>
+            <p className="font-normal text-[12px] text-[var(--gray-300)] ">
+              Log in using your e-mail and password registered
+            </p>
 
             <label htmlFor="email">Email</label>
             <input id="email" type="email" />
@@ -16,9 +22,11 @@ export function Login() {
 
             <button type="submit">Enter</button>
           </form>
-          <div>
-            <h1>Don't have an account yet?</h1>
-            <p>Register right now</p>
+          <div className="w-[400px] h-[161px] border border-gray-200 p-7 rounded">
+            <h1 className="text-xl font-bold">Don't have an account yet?</h1>
+            <p className="font-normal text-[12px] text-[var(--gray-300)]">
+              Register right now
+            </p>
             <button>Create Account</button>
           </div>
         </div>
