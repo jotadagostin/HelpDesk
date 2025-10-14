55aefa66b77e8cbd94aa649d351652546c254b65
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-10-14 10:54:20 +0200

    setting layout

--- helpdesk/src/pages/Admin/Admin.tsx
+++ helpdesk/src/pages/Admin/Admin.tsx
@@ -6,50 +6,61 @@ import avatarSvg from "../../assets/images/Avatar.svg";
 
 export function Admin() {
   return (
-    <div className="w-[] h-screen bg-[var(--gray-100)] flex">
-      <div className="w-[200px] h-screen bg-[var(--gray-100)] mt-3">
+    <div className="w-full h-screen bg-[var(--gray-100)] flex ">
+      <div className="w-[200px] h-screen bg-[var(--gray-100)]   flex flex-col">
         <img src={adminMenuSvg} alt="HelpDesk logo" />
-        <nav className="w-full  justify-center items-center">
+        <nav className="w-full  justify-center items-center pt-5">
           <ul className="">
             <li className="w-[168px] h-[44px] flex items-center gap-3 pl-5 justify-start">
-              <img
-                src={serviceSvg}
-                alt="cliboard icon"
-                className="w-[20px] h-[20px] "
-              />
-              <span className="text-[var(--gray-400)]">Calls</span>
+              <a href="" className="flex items-center justify-center gap-3">
+                <img
+                  src={clientSvg}
+                  alt="cliboard icon"
+                  className="w-[20px] h-[20px] "
+                />
+                <span className="text-[var(--gray-400)]">Calls</span>
+              </a>
             </li>
             <li className="w-[168px] h-[44px] flex items-center gap-3 pl-5 justify-start">
-              <img
-                src={techniciansSvg}
-                alt="cliboard icon "
-                className="w-[20px] h-[20px] color-[var(--gray-400)]"
-              />
-              <span className="text-[var(--gray-400)]">Technicians</span>
+              <a href="" className="flex items-center justify-center gap-3">
+                <img
+                  src={techniciansSvg}
+                  alt="cliboard icon "
+                  className="w-[20px] h-[20px] color-[var(--gray-400)]"
+                />
+                <span className="text-[var(--gray-400)]">Technicians</span>
+              </a>
             </li>
             <li className="w-[168px] h-[44px] flex items-center gap-3 pl-5 justify-start">
-              <img
-                src={clientSvg}
-                alt="cliboard icon "
-                className="w-[20px] h-[20px]"
-              />
-              <span className="text-[var(--gray-400)]">Clients</span>
+              <a href="" className="flex items-center justify-center gap-3">
+                <img
+                  src={clientSvg}
+                  alt="cliboard icon "
+                  className="w-[20px] h-[20px]"
+                />
+                <span className="text-[var(--gray-400)]">Clients</span>
+              </a>
             </li>
             <li className="w-[168px] h-[44px] flex items-center gap-3 pl-5 justify-start">
-              <img
-                src={serviceSvg}
-                alt="cliboard icon "
-                className="w-[20px] h-[20px]"
-              />
-              <span className="text-[var(--gray-400)]">Services</span>
+              <a href="" className="flex items-center justify-center gap-3">
+                <img
+                  src={serviceSvg}
+                  alt="cliboard icon "
+                  className="w-[20px] h-[20px]"
+                />
+                <span className="text-[var(--gray-400)]">Services</span>
+              </a>
             </li>
           </ul>
         </nav>
-        <div className="flex">
-          <img src={avatarSvg} alt="" />
-          <div>
-            <span>User Admin</span>
-            <p>user.adm.test.com</p>
+
+        <div className="h-[70%]  flex justify-center items-end ">
+          <div className="flex items-center gap-2 border-t border-t-[var(--gray-300)] py-5 px-4">
+            <img src={avatarSvg} alt="" />
+            <div className="">
+              <span>User Admin</span>
+              <p>user.adm@test.com</p>
+            </div>
           </div>
         </div>
       </div>
